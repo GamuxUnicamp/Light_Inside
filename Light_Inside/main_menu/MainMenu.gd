51665ddae00929120d64a65ef741a8fc2a0d49e0
Lelek Game Studio <lelekgamestudio@gmail.com>
2020-01-09 19:27:25 -0300

    Inicio Tutorial

--- Light_Inside/main_menu/MainMenu.gd
+++ Light_Inside/main_menu/MainMenu.gd
@@ -7,9 +7,13 @@ onready var exit_button = $ExitButton
 
 func _ready():
 	Global.OS = OS.get_name() #detectar OS
+	play_button.connect("pressed", self, "jogar")
 	options_button.connect("pressed",self,"opcoes")
 	credits_button.connect("pressed",self,"creditos")
 	exit_button.connect("pressed",self,"sair")
+
+func jogar():
+	get_tree().change_scene("res://levels/Tutorial.tscn")
 
 func opcoes():
 	get_tree().change_scene("res://main_menu/Opcoes.tscn")
